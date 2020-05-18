2315cbf435a2a5623ca5fba2815bbad69212cae3
honggangwang1979 <honggangwang1979@gmail.com>
2020-05-18 10:46:35 -0400

    Add files via upload

---
+++ IRG/Mfds.sh
@@ -0,0 +1,14 @@
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-43.992.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-43.992_0.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-44.108.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-44.458_0.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-44.225_0.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-44.225.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-44.342_0.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-44.458.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-44.342.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-44.575_0.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-44.108_0.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-44.575.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-43.875.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/IRG/G0_CDW_-43.875_0.fds
