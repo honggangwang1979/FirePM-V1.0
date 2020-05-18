5753a56ededee6fc64723e815031a6830758b6cb
honggangwang1979 <honggangwang1979@gmail.com>
2020-05-18 10:52:42 -0400

    Add files via upload

---
+++ ISG/Mfds.sh
@@ -0,0 +1,4 @@
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/ISG/G0_CDW_-44.325_0.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/ISG/G0_CDW_-44.175_0.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/ISG/G0_CDW_-44.175.fds
+sfds -n 2 -T 2 -t 40:40 -p long -i /work/hwang8/FPM_2/ISG/G0_CDW_-44.325.fds
